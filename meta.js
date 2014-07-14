2f352c8aaa13d65bca1558a0adbadb8af19d65ca
Jonathan Rajotte Julien <jonathan.rajotte-julien@ericsson.com>
2014-07-14 18:15:52 -0400

    Add new data structure and meta data. Multiple chart support

--- meta.js
+++ meta.js
@@ -1,33 +1,294 @@
 var MetaData = {
-	"osjvm":  {
-		"1": {
-			"description": "Linux / Openjdk7",
-			"os" : "linux",
-			"jvm": "openjdk7"
-		},
-		"2": {
-			"description": "Windows / Java 7",
-				"os" : "linux",
-				"jvm": "openjdk7"
-		},
-	},
-	"applicationComponents":  {
-		"experiment": {
-			"name": "Experiment Benchmark",
-			"tests": [
-				"chart1",
-				"chart2",
-				"chart3",
-				"chart4"
-				]
-		},
-		"lttng": {
-			"name": "Lttng Kernel Analysis",
-			"tests": [
-				"chart5",
-				"chart6",
-				"chart7"
-				]
+    "applicationComponents": {
+        "CTF Read & Seek Benchmark (500 seeks)": {
+            "name": "CTF Read & Seek Benchmark (500 seeks)",
+            "tests": [
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart0",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "CTF Read & Seek Benchmark (500 seeks):tr",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart15",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "CTF Read & Seek Benchmark (500 seeks):tr",
+                    "unit": "s"
+                }
+            ]
+        },
+        "CTF Read Benchmark": {
+            "name": "CTF Read Benchmark",
+            "tests": [
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart1",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "CTF Read Benchmark:trace-kernel",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart2",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "CTF Read Benchmark:trace-kernel-vm",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart16",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "CTF Read Benchmark:trace-kernel",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart17",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "CTF Read Benchmark:trace-kernel-vm",
+                    "unit": "s"
+                }
+            ]
+        },
+        "Event matching (time)": {
+            "name": "Event matching (time)",
+            "tests": [
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart3",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "Event matching (time):Match TCP events",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart18",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "Event matching (time):Match TCP events",
+                    "unit": "s"
+                }
+            ]
+        },
+        "Experiment benchmark": {
+            "name": "Experiment benchmark",
+            "tests": [
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart4",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "Experiment Benchmark traces:1",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart5",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "Experiment Benchmark traces:11",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart6",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "Experiment Benchmark traces: 136",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart7",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "Experiment Benchmark traces:19",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart8",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "Experiment Benchmark traces:3",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart9",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "Experiment Benchmark traces:32",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart10",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "Experiment Benchmark traces:52",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart11",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "Experiment Benchmark traces:6",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart12",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "Experiment Benchmark traces:84",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart19",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "Experiment Benchmark traces:1",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart20",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "Experiment Benchmark traces:11",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart21",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "Experiment Benchmark traces: 136",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart22",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "Experiment Benchmark traces:19",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart23",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "Experiment Benchmark traces:3",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart24",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "Experiment Benchmark traces:32",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart25",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "Experiment Benchmark traces:52",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart26",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "Experiment Benchmark traces:6",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart27",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "Experiment Benchmark traces:84",
+                    "unit": "s"
+                }
+            ]
+        },
+        "LTTng kernel analysis": {
+            "name": "LTTng kernel analysis",
+            "tests": [
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart13",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "LTTng Kernel Analysis: Trace2",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart28",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "LTTng Kernel Analysis: Trace2",
+                    "unit": "s"
+                }
+            ]
+        },
+        "Trace synchronization (time)": {
+            "name": "Trace synchronization (time)",
+            "tests": [
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart14",
+                    "jvm": "1.7",
+                    "os": "windows",
+                    "title": "Trace synchronization (time):Match TCP e",
+                    "unit": "s"
+                },
+                {
+                    "dimension": "CPU Time",
+                    "file": "chart29",
+                    "jvm": "1.7",
+                    "os": "linux",
+                    "title": "Trace synchronization (time):Match TCP e",
+                    "unit": "s"
+                }
+            ]
+        }
+    },
+    "osjvm": {
+        "1": {
+            "description": "linux / 1.7",
+            "jvm": "1.7",
+            "os": "linux"
+        },
+        "2": {
+            "description": "windows / 1.7",
+            "jvm": "1.7",
+            "os": "windows"
+        }
+    },
+	"overview": {
+		"dimension": "CPU Time",
+		"file": "chart_overview",
+		"jvm": "1.7",
+		"os": "windows",
+		"title": "Trace synchronization (time):Match TCP e",
+		"unit": "s"
 		}
-	}
 }
